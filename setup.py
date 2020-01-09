c392a823368a9c16be03807fffac93687c564609
zhijing <zhijing@mit.edu>
2020-01-09 18:39:10 +0800

    updated the pip version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='bleu',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     package_dir={'mypkg': 'bleu'},
     # package_data={'mypkg': ['data/*.json']},
